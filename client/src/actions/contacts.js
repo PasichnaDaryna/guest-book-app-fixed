8ec7e94d51b4b09957dc409d6b72e48a747dd4fb
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-04-29 15:28:54 +0300

    update actions and selectors

--- client/src/actions/contacts.js
+++ client/src/actions/contacts.js
@@ -1,42 +1,5 @@
 import * as api from '../api'
 
-
-//Action creators
-
-// export const getContacts = () => async (dispatch) => {
-
-
-//     try {
-//         const { data } = await api.fetchContacts()
-//         dispatch({ type: "FETCH_ALL", payload: data })
-//     }
-//     catch (error) {
-//         console.log(error.message)
-//     }
-
-
-// }
-
-
-// export const createContact = (contact) => async (dispatch) => {
-//     try {
-//         const { data } = await api.createContact(contact)
-//         dispatch({ type: 'CREATE', payload: data })
-
-//     } catch (error) {
-//         console.log(error)
-
-//     }
-// }
-
-
-// end
-
-// new below
-
-
-
-
 import { createAction } from "@reduxjs/toolkit";
 
 
