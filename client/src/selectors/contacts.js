7e3531f7048c1eaa6c1c8bf91580f53ffe95c7bb
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-04-29 15:30:00 +0300

    fix selector

--- client/src/selectors/contacts.js
+++ client/src/selectors/contacts.js
@@ -1,4 +1,4 @@
-export const getContacts = (state) => [state.contacts.items];
+export const getContacts = (state) => state.contacts.items;
 
 export const getLoader = (state) => state.contacts.loading;
 
