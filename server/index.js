81685d05a96b4741b66e616fbba9687ff242cfad
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-05-05 19:40:24 +0300

    trying to fix heroku deploy

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from 'cors';
+import path from 'path';
 
 
 import contactsRoutes from './routes/contacts.js'
@@ -9,10 +10,6 @@ import contactsRoutes from './routes/contacts.js'
 
 
 const app = express();
-
-
-
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
@@ -25,3 +22,15 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
     .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false)
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
